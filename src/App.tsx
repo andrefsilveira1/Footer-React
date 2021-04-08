54a9eee187c6d52c177b096068c57f002c96f20e
unknown <freitasandre38@gmail.com>
2021-04-08 20:02:27 -0300

    Nova alteração

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './Styles/Global.css';
 import './Styles/Home.css';
 import './App.css';
-import { Header} from './Components/Header'
 import { Footer } from './Components/Footer';
 
 function App() {
